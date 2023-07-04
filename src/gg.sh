150167acf1cb756ff0938f46821303c61fcc63b8
Boris Vasilyev <boris.vasilyev@tmhsmart.ru>
2023-07-04 06:24:35 +0300

    develop-v1.0.HMI-1-ggg-status: (3)

--- src/gg.sh
+++ src/gg.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo not iplemented
+declare iArg
+declare -a gitArguments
+
+for iArg in "$@"
+do
+	#echo "[$iArg]"
+	if [ "$iArg" == --help ]
+	then
+		echo '
+gg - обёртка над системной утилитой git. Обеспечивает следующие вещи:
+  * делает более прозрачной работу с git submodules
+  * добавляет сущность task и приводит формирование веток к взятию-вливанию задач
+  * экранирует опасные команды git, такие как "git rebase"
+
+Получить текущее состояние всех git-submoodules текущего репозитория - gg status
+'
+		exit 0
+	fi
+done
+
+for 
